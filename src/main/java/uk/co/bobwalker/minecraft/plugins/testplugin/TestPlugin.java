ae673f763d7291daee73dee2984b7fb9ecc545a6
Bob Walker <rjewalker@gmail.com>
2015-11-22 22:22:58 +0000

    Git setup files and whitespace fixes

--- src/main/java/uk/co/bobwalker/minecraft/plugins/testplugin/TestPlugin.java
+++ src/main/java/uk/co/bobwalker/minecraft/plugins/testplugin/TestPlugin.java
@@ -7,24 +7,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPlugin extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		getLogger().info("onEnable has been invoked!");
-	}
-	
-	@Override
-	public void onDisable() {
-		getLogger().info("onDisable has been invoked!");
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		getLogger().info("onCommand has been invoked");
-		if (command.getName().equalsIgnoreCase("awesome") && sender instanceof Player) {
-			Player player = (Player)sender;
-			player.sendMessage(String.format("Hello %s, your Dad is awesome!", player.getName()));
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable has been invoked!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable has been invoked!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        getLogger().info("onCommand has been invoked");
+        if (command.getName().equalsIgnoreCase("awesome") && sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage(String.format("Hello %s, your Dad is awesome!", player.getName()));
+            player.setFlySpeed(100);
+            return true;
+        }
+        return false;
+    }
 }
